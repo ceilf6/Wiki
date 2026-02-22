5cab75de958338108935cd8e0f066e3c361ff2e0
WJH <3506456886@qq.com>
2026-02-22 20:39:57 +0800

    feat[ptr]: useState, useReducer 源码分析: useState 的本质就是 useReducer 的一个简化版 - 其 reducer 是内置的基础版而不是用户传入的

--- React/sandboxs/src/App.jsx
+++ React/sandboxs/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 // import {CheckBoxGroupText,SelectText ,RadioBoxGroupText} from './components'
 // import Comp from './test/RefTestFunc'
 // import ForwardRefTest from './test/ForwardRefTest copy'
@@ -8,12 +8,25 @@ import React from 'react'
 // import EventTest from './Source-Event/Test'
 import UseReducerText from './test/UseReducerText'
 
+const UseStateText = (props) => {
+    const {initialData} = props
+
+    const [state,setState] = useState(() => 666*initialData)
+    return (
+        <button onClick={() => setState(state*10)}>
+            {state}
+        </button>
+    )
+}
+
 const el = (
     <>
         {/* <CheckBoxGroupText />
         <SelectText />
         <RadioBoxGroupText /> */}
+        
         <UseReducerText />
+        <UseStateText initialData={1}/>
     </>
 )
 
