216e9acd5353b9d9df61641600015c6c17f37a76
WJH <3506456886@qq.com>
2026-03-04 22:22:16 +0800

    feat: base debounce

---
+++ JS/algorithm/debounce.js
@@ -0,0 +1,9 @@
+function debounce(fn, wait) {
+    let timer = null
+    return function () {
+        if (timer) {
+            clearTimeout(timer)
+        }
+        timer = setTimeout(fn, wait)
+    }
+}
